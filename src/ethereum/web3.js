f99a04a57708ddc141314636f33c20bf6f7305c9
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-09-03 20:39:52 -0400

    whitespace fixed

--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -3,35 +3,35 @@ import Web3 from 'web3';
 let web3;
 
 function networkCheck(){
-	web3.eth.net.getNetworkType((err, netId) => {
-		switch (netId) {
-			case "main":
-				alert("This is Mainnet. Please switch to Rinkeby Test Network!");
-				break
-		  	case "ropsten":
-				alert("This is Ropsten test network. Please switch to Rinkeby Test Network!");
-				break
-		  	case "rinkeby":
-				alert("Nice! You're now connected to Rinkeby Test Network!");
-				break
-			case "kovan":
-				alert("This is Kovan test network. Please switch to Rinkeby Test Network!");
-				break
-			default:
-				alert('This is an unknown network. Please connect to Rinkeby Test Network!');
-		}
-	  })
-	}
+  web3.eth.net.getNetworkType((err, netId) => {
+    switch (netId) {
+      case "main":
+        alert("This is Mainnet. Please switch to Rinkeby Test Network!");
+        break
+      case "ropsten":
+        alert("This is Ropsten test network. Please switch to Rinkeby Test Network!");
+        break
+      case "rinkeby":
+        alert("Nice! You're now connected to Rinkeby Test Network!");
+        break
+      case "kovan":
+        alert("This is Kovan test network. Please switch to Rinkeby Test Network!");
+        break
+      default:
+        alert('This is an unknown network. Please connect to Rinkeby Test Network!');
+    }
+    })
+  }
 
 
 
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-    // We are in the browser and metamask is running.
-    web3 = new Web3(window.web3.currentProvider);
+  // We are in the browser and metamask is running.
+  web3 = new Web3(window.web3.currentProvider);
 }else{
-    // We are on the server *OR* the user is not running metamask
-    const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/fmYYH0aPRKNF5MFSuVNH');
-    web3 = new Web3(provider);
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/fmYYH0aPRKNF5MFSuVNH');
+  web3 = new Web3(provider);
 }
 
 networkCheck();
